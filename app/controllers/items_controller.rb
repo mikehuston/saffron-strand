634f7c10462f99f0779e6d87a9bf43d8e7611912
prathamalag1994 <prathamalag@berkeley.edu>
2015-03-08 12:59:23 -0700

    removed test items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,6 @@ class ItemsController < ApplicationController
 
 	def index
 		@items = Item.all
-		po = Item.create(name: 'pea', price: 3.4, category: 'breakfast')
-		po = Item.create(name: 'ice', price: 6.4, category: 'breakfast')
-		po = Item.create(name: 'rice', price: 3.6, category: 'lunch')
-		po = Item.create(name: 'dri', price: 3.9, category: 'lunch')
-		po = Item.create(name: 'pie', price: 2.4, category: 'dinner')
-		po = Item.create(name: 'ham', price: 31.4, category: 'dinner')
 	end
 
 	def breakfast
