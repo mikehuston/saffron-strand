8d2e1935a6ef25fcbab4b8f02f21300948b0ee4a
saasbook <saasbook@saasbook.(none)>
2015-03-08 19:51:36 -0300

    actually added admin folders

---
+++ test/functional/admin/menu_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::MenuControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/admin/items_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::ItemsHelperTest < ActionView::TestCase
+end
