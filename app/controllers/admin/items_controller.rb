76e2f0c3ca2b289df82a89bc86f1745ba28c8a0a
saasbook <saasbook@saasbook.(none)>
2015-03-08 23:16:05 -0300

    admin CRUD done

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,20 +3,31 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create!(params[:item])
+    redirect_to admin_items_path
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find params[:id]
+    @item.update_attributes!(params[:item])
+    redirect_to admin_items_path
   end
 
   def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to admin_items_path
   end
 
   def index
+    @items = Item.all
   end
 
-  def show
+  def show 
+    @item = Item.find(params[:id])
   end
 end
