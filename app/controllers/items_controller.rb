5071b191109d1a24ac5b6f650a92df3fc0a219bb
saasbook <saasbook@saasbook.(none)>
2015-03-10 21:52:10 -0300

    minor fixes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,15 +5,15 @@ class ItemsController < ApplicationController
 	end
 
 	def breakfast
-		@items = Item.where(category: 'breakfast')
+		@items = Item.where(category: 'Breakfast')
 	end
 
 	def lunch
-		@items = Item.where(category: 'lunch')
+		@items = Item.where(category: 'Lunch')
 	end
 
 	def dinner
-		@items = Item.where(category: 'dinner')
+		@items = Item.where(category: 'Dinner')
 	end
 
 end
