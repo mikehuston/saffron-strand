bb7d75935a829d29bd6b6c3bf8c763cb848ce47d
prathamalag1994 <prathamalag@berkeley.edu>
2015-03-18 13:15:25 -0700

    partial bug fix

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ItemsController < ApplicationController
+  before_filter :authenticate_user!
   def new
   end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-  	@admin = current_user.admin
-  	if @admin
-  		redirect_to admin_items_path
+  	if current_user != nil
+  		@admin = current_user.admin
+  		if @admin
+  			redirect_to admin_items_path
+  		end
   	end
   end
 end
