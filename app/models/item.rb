184836637d4f4071ac4f54c802e781e2400276a2
Michael Huston <michael.huston@comcast.net>
2015-03-30 14:39:41 -0700

    Add migration for items table and update items model to allow for attachment of image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
-  attr_accessible :category, :desc, :name, :price
+  attr_accessible :category, :desc, :name, :price, :image
+  has_attached_file :avatar,
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :dropbox_visibility => 'public'
 end
