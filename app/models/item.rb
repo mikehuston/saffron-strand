1b4fff3c05da39d08c66a2758d781677092740f0
Michael Huston <michael.huston@comcast.net>
2015-03-30 15:01:20 -0700

    Update item new and edit views to support image upload to Dropbox

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ActiveRecord::Base
   attr_accessible :category, :desc, :name, :price, :image
-  has_attached_file :avatar,
+  has_attached_file :image,
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :dropbox_visibility => 'public'
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
