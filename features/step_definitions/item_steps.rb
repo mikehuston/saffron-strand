985d14a67608758e0cf83dcab4f7770f97f6f2c6
Michael Huston <michael.huston@comcast.net>
2015-03-30 18:28:15 -0700

    Add happy and sad path for testing image upload

--- features/step_definitions/item_steps.rb
+++ features/step_definitions/item_steps.rb
@@ -11,3 +11,21 @@ end
 Then /^I should not see the "(.*)" link/ do |link|
 	page.should_not have_link(link)
 end
+
+When /^I upload an image file$/ do
+	attach_file(:item_image, File.join(Rails.root, 'features/data', 'turkeysandwich.jpeg'))
+end
+
+When /^I upload a file that is not an image$/ do
+	attach_file(:item_image, File.join(Rails.root, 'features/data', 'pdf-sample.pdf'))
+end
+
+Then /^the "(.*)" item should have an image$/ do |item_name|
+	item = Item.where(name: item_name)[0]
+	assert(item.image != nil)
+end
+
+Then /^the "(.*)" item should not be valid$/ do |item_name|
+	item = Item.where(name: item_name)[0]
+	assert(item.valid? == false)
+end
