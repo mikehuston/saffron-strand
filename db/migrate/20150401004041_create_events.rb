45250847599ae0877475568d57a0ec7574bae7eb
saasbook <saasbook@saasbook.(none)>
2015-03-31 22:11:23 -0300

    migrations

---
+++ db/migrate/20150401004041_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration
+
+	def change
+		create_table :events do |t|
+			t.string :name
+			t.integer :head_count
+			t.references :user
+		end
+	end
+  def up
+  end
+
+  def down
+  end
+end
