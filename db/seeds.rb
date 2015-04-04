c58b64b252f4c12990b8124695854078b61f5873
saasbook <saasbook@saasbook.(none)>
2015-04-03 21:51:44 -0300

    added some seed items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.create(name: 'Chicken Parm', desc: 'delish food', price: 10, category: 'Dinner', food_type: 'Meat')
+Item.create(name: 'Eggs and Bacon', desc: 'American breakfast', price: 6, category: 'Breakfast', food_type: 'Meat')
+Item.create(name: 'Roast Beef Sandwich', desc: 'better than Subway', price: 7, category: 'Lunch', food_type: 'Meat')
+Item.create(name: 'Caesar salad', desc: 'healthy side for your meal', price: 4, category: 'Dinner', food_type: 'Side')
+
