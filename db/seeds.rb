6eeee4b3b2e2b913dd82ec2d37dac47a3021bc7e
Michael Huston <michael.huston@comcast.net>
2015-04-16 14:36:03 -0700

    Update seed file with items provided by Saffron Strand

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,5 +25,66 @@ menu2.items << item4
 menu2.items << item7
 menu2.items << item5
 
+appetizers = ["Vegetable Plate",
+			"Fruit Plate",
+			"Cheese Plate",
+			"Chips and Salsa",
+			"Chicken and Shrimp Lumpia",
+			"Sundried Tomato and Basil Tea Sandwiches",
+			"Smoked Salmon Tea Sandwiches",
+			"Almond Chicken Salad Tea Sandwiches",
+			"Stuffed Mushrooms",
+			"Sweet and Sour Meatballs",
+			"Tuna Cutlets"]
+
+appetizers.each do |a|
+	Item.create name: a, food_type: 'Appetizer'
+end
+
+entrees = ["Roasted Chicken with Lemon Herb Sauce",
+		"Fiesta Shredded Chicken",
+		"Asian Flank Steak",
+		"Pork Carnitas",
+		"Polynesian Pulled Pork",
+		"Meat Lasagna",
+		"Vegetable Lasagna"]
+
+entrees.each do |e|
+	Item.create name: e, food_type: 'Entree'
+end
+
+sides = ["Spiced Green Beans",
+		"Almond Green Beans",
+		"Corn Casserole",
+		"Vegetarian Dirty Rice",
+		"Wild Rice with Mushrooms",
+		"Toasted Almond Currant Rice Pilaf",
+		"Indian Fried Rice",
+		"Pesto Pasta",
+		"Pasta al Pomodoro",
+		"Roasted Root Vegetables",
+		"Spinach Apple Salad",
+		"Sesame Peanut Noodle Salad",
+		"Cucumber Salad",
+		"Carrot Raisin Salad",
+		"Firecracker Salad",
+		"Cranberry Almond Salad",
+		"Tri-Color Slaw",
+		"Asian Style Slaw",
+		"Miso Cucumber Salad"]
+
+sides.each do |s|
+	Item.create name: s, food_type: 'Side'
+end
+
+desserts = ["Blueberry Crumble",
+			"Peach Cobbler",
+			"Lemon Bundt Cake"]
+
+desserts.each do |d|
+	Item.create name: d, food_type: 'Dessert'
+end
+
+User.destroy_all :email =~ /@saffron.test/
 User.create!(:email => 'testuser@saffron.test', :password => 'password', :password_confirmation => 'password')
 User.create!(:email => 'testadmin@saffron.test', :password => 'password', :password_confirmation => 'password', :admin => true)
