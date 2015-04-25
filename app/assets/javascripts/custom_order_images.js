d086ba7feb5f930278b25b7bbcb22acee74e80fd
Michael Huston <michael.huston@comcast.net>
2015-04-25 16:34:16 -0700

    Incorporate ajax loading of images with new custom order front end

--- app/assets/javascripts/custom_order_images.js
+++ app/assets/javascripts/custom_order_images.js
@@ -1,13 +1,16 @@
 var ImagePopup = {
   setup: function() {
     // add hidden 'div' to end of page to display popup:
-    var popupDiv = $('<div id="itemInfo" style="padding: 2ex; position: absolute; border: 2px double grey; backgroud: wheat;"></div>');
-    popupDiv.hide().appendTo($('body'));
-    $('.item').click(ImagePopup.getImageInfo);
-    $('#closeLink').click(ImagePopup.hideImageInfo);
+    // var popupDiv = $('<div id="itemInfo" style="padding: 2ex; position: absolute; border: 2px double grey; backgroud: wheat;"></div>');
+    // popupDiv.hide().appendTo($('body'));
+    // $('span').click(ImagePopup.getImageInfo);
+    // $('span').mouseenter(ImagePopup.getImageInfo);
+    $('.item').mouseover(ImagePopup.getImageInfo);
+    // $('#closeLink').click(ImagePopup.hideImageInfo);
   }
   ,getImageInfo: function() {
-    console.log('here');
+    console.log("HELLO");
+    console.log($(this));
     $.ajax({type: 'GET',
             url: $(this).attr('href'),
             timeout: 5000,
@@ -19,18 +22,17 @@ var ImagePopup = {
   }
   ,showImageInfo: function(data, requestStatus, xhrObject) {
     // center a floater 1/2 as wide and 1/4 as tall as screen
-    var oneFourth = Math.ceil($(window).width() / 4);
-    $('#itemInfo').
-      css({'left': oneFourth,  'width': 2*oneFourth, 'top': 250}).
+    // var oneFourth = Math.ceil($(window).width() / 4);
+    $('#displayImage').
       html(data).
       show();
     // make the Close link in the hidden element work
     $('#closeLink').click(ImagePopup.hideImageInfo);
     return(false);  // prevent default link action
   }
-  ,hideImageInfo: function() {
-    $('#itemInfo').hide();
-    return(false);
-  }
+  // ,hideImageInfo: function() {
+  //   $('#itemInfo').hide();
+  //   return(false);
+  // }
 };
 $(ImagePopup.setup);
