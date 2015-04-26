6a19aec96bb546249ab7b708413ffac289b42545
Michael Huston <michael.huston@comcast.net>
2015-04-25 17:16:47 -0700

    Clean up JS code

--- app/assets/javascripts/custom_order_images.js
+++ app/assets/javascripts/custom_order_images.js
@@ -1,16 +1,8 @@
 var ImagePopup = {
   setup: function() {
-    // add hidden 'div' to end of page to display popup:
-    // var popupDiv = $('<div id="itemInfo" style="padding: 2ex; position: absolute; border: 2px double grey; backgroud: wheat;"></div>');
-    // popupDiv.hide().appendTo($('body'));
-    // $('span').click(ImagePopup.getImageInfo);
-    // $('span').mouseenter(ImagePopup.getImageInfo);
     $('.item').mouseover(ImagePopup.getImageInfo);
-    // $('#closeLink').click(ImagePopup.hideImageInfo);
   }
   ,getImageInfo: function() {
-    console.log("HELLO");
-    console.log($(this));
     $.ajax({type: 'GET',
             url: $(this).attr('href'),
             timeout: 5000,
@@ -21,18 +13,10 @@ var ImagePopup = {
     return(false);
   }
   ,showImageInfo: function(data, requestStatus, xhrObject) {
-    // center a floater 1/2 as wide and 1/4 as tall as screen
-    // var oneFourth = Math.ceil($(window).width() / 4);
     $('#displayImage').
       html(data).
       show();
-    // make the Close link in the hidden element work
-    $('#closeLink').click(ImagePopup.hideImageInfo);
     return(false);  // prevent default link action
   }
-  // ,hideImageInfo: function() {
-  //   $('#itemInfo').hide();
-  //   return(false);
-  // }
 };
 $(ImagePopup.setup);
