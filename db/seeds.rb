b02aafa7650797623f2d9f4670b7f44f31609fb0
Michael Huston <michael.huston@comcast.net>
2015-04-26 17:56:23 -0700

    Update seeds.rb, custom order UI fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,40 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-item1 = Item.create(name: 'Chicken Parm', desc: 'delish food', price: 10, category: 'Dinner', food_type: 'Meat')
-item2 = Item.create(name: 'Eggs and Bacon', desc: 'American breakfast', price: 6, category: 'Breakfast', food_type: 'Meat')
-item3 = Item.create(name: 'Roast Beef Sandwich', desc: 'better than Subway', price: 7, category: 'Lunch', food_type: 'Meat')
-item4 = Item.create(name: 'Caesar Salad', desc: 'healthy side for your meal', price: 4, category: 'Dinner', food_type: 'Side')
-item5 = Item.create(name: 'Filet Mignon', desc: 'grass fed beef', price: 12, category: 'Dinner', food_type: 'Meat')
-item6 = Item.create(name: 'Mixed Green Salad', desc: 'healthy side for your meal', price: 4, category: 'Dinner', food_type: 'Side')
-item7 = Item.create(name: 'Green Beans', desc: 'fresh and local', price: 4, category: 'Lunch', food_type: 'Side')
 
+User.destroy_all
+Event.destroy_all
+Menu.destroy_all
+Item.destroy_all
 
-menu1 = Menu.create name: 'Sample $8 Menu', budget_per_person: 8, sample: true
-menu1.items << item6
-menu1.items << item7
-menu1.items << item4
+# item1 = Item.create(name: 'Chicken Parm', desc: 'delish food', price: 10, category: 'Dinner', food_type: 'Meat')
+# item2 = Item.create(name: 'Eggs and Bacon', desc: 'American breakfast', price: 6, category: 'Breakfast', food_type: 'Meat')
+# item3 = Item.create(name: 'Roast Beef Sandwich', desc: 'better than Subway', price: 7, category: 'Lunch', food_type: 'Meat')
+# item4 = Item.create(name: 'Caesar Salad', desc: 'healthy side for your meal', price: 4, category: 'Dinner', food_type: 'Side')
+# item5 = Item.create(name: 'Filet Mignon', desc: 'grass fed beef', price: 12, category: 'Dinner', food_type: 'Meat')
+# item6 = Item.create(name: 'Mixed Green Salad', desc: 'healthy side for your meal', price: 4, category: 'Dinner', food_type: 'Side')
+# item7 = Item.create(name: 'Green Beans', desc: 'fresh and local', price: 4, category: 'Lunch', food_type: 'Side')
 
-menu2 = Menu.create name: 'Sample $15 Menu', budget_per_person: 15, sample: true
-menu2.items << item4
-menu2.items << item7
-menu2.items << item5
+# menu1 = Menu.create name: 'Sample $8 Menu', budget_per_person: 8, sample: true
+# menu1.items << item6
+# menu1.items << item7
+# menu1.items << item4
+
+# menu2 = Menu.create name: 'Sample $15 Menu', budget_per_person: 15, sample: true
+# menu2.items << item4
+# menu2.items << item7
+# menu2.items << item5
+
+def create_item_helper name, food_type
+	item = Item.create name: name, food_type: food_type
+	fname = 'app/assets/images/' + name.downcase.gsub(' ', '_') + '.jpg'
+	begin
+		item.image = Rails.root.join(fname).open
+		item.save!
+	rescue Exception => e
+		puts e
+	end
+end
 
 appetizers = ["Vegetable Plate",
 			"Fruit Plate",
@@ -37,8 +53,8 @@ appetizers = ["Vegetable Plate",
 			"Sweet and Sour Meatballs",
 			"Tuna Cutlets"]
 
-appetizers.each do |a|
-	Item.create name: a, food_type: 'Appetizer'
+appetizers.each do |i|
+	create_item_helper i, 'Appetizer'
 end
 
 entrees = ["Roasted Chicken with Lemon Herb Sauce",
@@ -49,8 +65,8 @@ entrees = ["Roasted Chicken with Lemon Herb Sauce",
 		"Meat Lasagna",
 		"Vegetable Lasagna"]
 
-entrees.each do |e|
-	Item.create name: e, food_type: 'Entree'
+entrees.each do |i|
+	create_item_helper i, 'Entree'
 end
 
 sides = ["Spiced Green Beans",
@@ -73,8 +89,8 @@ sides = ["Spiced Green Beans",
 		"Asian Style Slaw",
 		"Miso Cucumber Salad"]
 
-sides.each do |s|
-	Item.create name: s, food_type: 'Side'
+sides.each do |i|
+	create_item_helper i, 'Side'
 end
 
 desserts = ["Blueberry Crumble",
