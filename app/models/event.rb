5988c2479fce3044f3a5fa295554bcc15c202d41
Matthew Nguyen <matthewnguyen@berkeley.edu>
2015-04-27 21:18:49 -0300

    changed type to event_type

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :type, :head_count
+  attr_accessible :event_type, :head_count
   has_one :menu, :dependent => :destroy
   belongs_to :user
 
@@ -8,13 +8,13 @@ class Event < ActiveRecord::Base
   end
 
   def self.get_event_types
-  	['Breakfast','Brunch','Lunch','Dinner']
+  	['Cocktail Party','Breakfast','Brunch','Lunch','Dinner']
   end
 
   def self.null_event
   	@@instance ||= Event.new
   	class << @@instance
-  		def type
+  		def event_type
   			"You haven't created any events yet. Click here to begin making a customized menu for your event!"
   		end
   		def show
