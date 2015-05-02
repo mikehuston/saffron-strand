de118ff4a4db5edc21ec24714281bc117635d481
prathamalag1994 <prathamalag@berkeley.edu>
2015-05-02 04:33:51 -0700

    logic for emailing

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,3 +1,5 @@
+require 'mandrill'
+
 class Admin::EventsController < ApplicationController
 
 	def index
@@ -6,10 +8,37 @@ class Admin::EventsController < ApplicationController
 		@eventsc = Event.where(status: 'final')
 	end
 
+	def email_body user
+    "Hi! \n \t #{user.name}! Thank you for your order. Payment has been received and it has been finalised."
+ 	end
+
 	def change
 		@event = Event.find(params[:id])
+		@user = @event.user
 		@event.status = 'confirm'
 		@event.save!
+		@price = @event.head_count * @event.menu.budget_per_person
+		mandrill = Mandrill::API.new ENV["MANDRILL_API_KEY"]
+    	message = {  
+     	:subject=> "Order Confirmation",  
+     	:from_name=> "Saffron Strand",  
+     	:html=> "<h3> Your order has been confirmed! Here is your invoice:</h3><br>
+     	<h4><%= @event.event_type %><br>
+     	<%= @event.menu.budget_per_person %> Per Person <br>
+     	<%= @event.head_count %>  People<br>
+     	<%= @price %> is your total cost!<br> Items:</h4> <br>
+        <% @event.menu.items.each do |item| %>
+        <%= item.name %> <br>
+  		<% end %>",  
+     	:to=>[  
+       	{  
+       	  :email=> @user.email,  
+       	  :name=> @user.name 
+       	}  
+     	],    
+     	:from_email=>"Saffron@saffronstrand.com"  
+    	}  
+    	sending = mandrill.messages.send message  
 		redirect_to admin_events_path
 	end
 
@@ -17,6 +46,20 @@ class Admin::EventsController < ApplicationController
 		@event = Event.find(params[:id])
 		@event.status = 'final'
 		@event.save!
+		mandrill = Mandrill::API.new ENV["MANDRILL_API_KEY"]
+    	message = {  
+     	:subject=> "Order Fianlized",  
+     	:from_name=> "Saffron Strand",  
+     	:html=> email_body(@user),  
+     	:to=>[  
+       	{  
+       	  :email=> @user.email,  
+       	  :name=> @user.name 
+       	}  
+     	],    
+     	:from_email=>"Saffron@saffronstrand.com"  
+    	}  
+    	sending = mandrill.messages.send message 
 		redirect_to admin_events_path
 	end
 
