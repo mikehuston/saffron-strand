96d41c5561512c8553eb010385cf9827b2f6be34
Michael Huston <michael.huston@comcast.net>
2015-05-04 12:24:45 -0700

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
   has_one :item, :dependent => :destroy
+
+  def self.names
+    ['Cocktail Party', 'Brunch', 'Lunch', 'Dinner']
+  end
+
 end
